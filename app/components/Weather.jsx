9f649d97be8d471c87d6162c23c5a94dc02640cd
Jose Carlos Ustra Junior <dev@ustrajunior.com>
2016-11-15 10:10:04 -0300

    Add weather component with fake data

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -1,9 +1,30 @@
 var React = require('react');
+var WeatherForm = require('WeatherForm');
+var WeatherMessage = require('WeatherMessage');
 
 var Weather = React.createClass({
+    getInitialState: function() {
+        return {
+            location: 'Miami',
+            temp: 36
+        }
+    },
+    handleSearch: function(location) {
+        this.setState({
+            location: location,
+            temp: 32
+        });
+    },
     render: function() {
+        var {temp, location} = this.state;
+
         return (
-            <h3>Weather component</h3>
+            <div>
+                <h3>Get Weather</h3>
+                <WeatherForm onSearch={this.handleSearch} />
+                <WeatherMessage location={location} temp={temp}/>
+            </div>
+            
         )
     }
 });
---
+++ app/components/WeatherMessage.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var WeatherMessage = React.createClass({
+    render: function() {
+        var {temp, location} = this.props;
+
+        return (
+            <h2>It's it {temp} in {location}.</h2>
+        )
+    }
+});
+
+module.exports = WeatherMessage;
