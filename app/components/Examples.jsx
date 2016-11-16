54ba7042ab5d6ecbcb1f87d5d578a208123e7879
Jose Carlos Ustra Junior <dev@ustrajunior.com>
2016-11-16 20:56:59 -0300

    Style examples page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,9 +1,17 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
-    return (
-        <h3>Examples component</h3>
-    )
+  return (
+    <div>
+      <h1>Examples</h1>
+      <p>Here are a few example locations:</p>
+      <ol>
+        <li><Link to='/?location=Natal'>Natal, Brazil</Link></li>
+        <li><Link to='/?location=Toronto'>Toronto, Canada</Link></li>
+      </ol>
+    </div>
+  );
 }
 
 module.exports = Examples;
