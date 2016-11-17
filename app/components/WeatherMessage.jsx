5e733121985a55526672e64eafcfc84cd1c1533c
Jose Carlos Ustra Junior <dev@ustrajunior.com>
2016-11-16 21:06:32 -0300

    Style weather page

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,9 +1,9 @@
 var React = require('react');
 
 var WeatherMessage = ({temp, location}) => {
-    return (
-        <h2>It's it {temp} in {location}.</h2>
-    )
+  return (
+    <h3 className="text-center">It's it {temp}in {location}.</h3>
+  )
 }
 
 module.exports = WeatherMessage;
